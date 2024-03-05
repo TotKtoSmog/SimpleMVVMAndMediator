69836f5406419a2b0af7e7b3daf3f62a1ea5fea8
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-03-05 22:34:52 +0300

    Подключение MainWindowViewModel к MainWindow

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,44 @@
-﻿namespace SimpleMVVMAndMediator.ViewModels
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SimpleMVVMAndMediator.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        private string _sourcePage1;
+        public string SourcePage1
+        {
+            get 
+            { 
+                return _sourcePage1; 
+            }
+            set 
+            { 
+                _sourcePage1 = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _sourcePage2;
+        public string SourcePage2
+        {
+            get
+            {
+                return _sourcePage2;
+            }
+            set
+            {
+                _sourcePage2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            SourcePage1 = "Page1.xaml";
+            SourcePage2 = "Page2.xaml";
+        }
     }
 }
