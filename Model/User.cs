24938f98ee8b62bd83c45763aaacfba2cc9a5948
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-03-05 22:41:50 +0300

    Создание стркуктуры для передачи сообщений

--- Model/User.cs
+++ Model/User.cs
@@ -2,5 +2,14 @@
 {
     public class User
     {
+        public string Name;
+        public string Address;
+        public string Description;
+        public User(string name, string address, string description) 
+        {
+            Name = name;
+            Address = address;
+            Description = description;
+        }
     }
 }
