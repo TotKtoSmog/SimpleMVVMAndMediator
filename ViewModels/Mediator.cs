464943ef5ba9317019977dcc54b60b7599c2a439
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-03-05 23:07:19 +0300

    Добавление DelegateCommand и Mediator

---
+++ ViewModels/Mediator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimpleMVVMAndMediator.ViewModels
+{
+    public class Mediator
+    {
+        private static Mediator instance;
+        private Mediator() { }
+
+        public static Mediator Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Mediator();
+                return instance;
+            }
+        }
+        public event Action<string, string> ReceivedMessage;
+        public void SendMessage(string receiver, string message)
+            => ReceivedMessage?.Invoke(receiver, message);
+
+        public event Action<string> ReceivedMessageFromPage1;
+        public void SendMessagePage2(string message)
+            => ReceivedMessageFromPage1?.Invoke(message);
+
+        public event Action<string> ReceivedMessageFromPage2;
+        public void SendMessagePage1(string message)
+            => ReceivedMessageFromPage2?.Invoke(message);
+    }
+}
